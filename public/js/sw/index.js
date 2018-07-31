45f4d6f5d1455bdc9de6cfb439edf3b81a497e5f
americanman <snowboardracer7@gmail.com>
2018-07-31 12:12:04 -0400

    Filter request that endsWith .jpg extention

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,8 @@
 self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
-});
+  console.log(event)
+  if(event.request.url.endsWith('.jpg')){
+     event.respondWith(
+       fetch('/imgs/dr-evil.gif')
+    )
+  }
+ });
